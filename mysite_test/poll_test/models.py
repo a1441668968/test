1c9f0df0be0493f15bc226e8ebc8b38e8bfa4eed
root <root@room9pc01.tedu.cn>
2018-09-10 22:13:09 +0800

    9.10

--- mysite_test/poll_test/models.py
+++ mysite_test/poll_test/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from datetime import timedelta
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     publish_date = models.DateTimeField()
+
     def __str__(self):
         return self.question_text
+
+    def recently(self):
+        return self.publish_date >= timezone.now() - timedelta(days=1)
 
 
 class Choice(models.Model):
--- mysite_test/poll_test/views.py
+++ mysite_test/poll_test/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question
 
 
 def polls_index(request):
-    return render(request, 'polls/poll_test_index.html')
+    # return render(request, 'polls/poll_test_index.html')
+    last_question_list = Question.objects.order_by('-publish_date')[:5]
+    context = {'question_list': last_question_list}
+    return render(request=request, template_name='polls/poll_test_index.html', context=context)
+
+
+def detail(request, q_id):
+    # return render(request=request, template_name='polls/detail.html', context={'question_id': q_id})
+    question = get_object_or_404(Question, id=q_id)
+    return render(request=request, template_name='polls/detail.html', context={'question': question})
+
+
+def result(request, q_id):
+    # return render(request=request, template_name='polls/result.html', context={'question_id': q_id})
+    question = get_object_or_404(Question, id=q_id)
+    return render(request=request, template_name='polls/result.html', context={'question': question})
+
+
+def vote(request, q_id):
+    question = get_object_or_404(Question, id=q_id)
+    choice_id = request.POST.get('choice_id')
+    choice = question.choice_set.get(id=choice_id)
+    choice.votes += 1
+    choice.save()
+    return redirect('result', q_id=q_id)  # 'result'对应urls.py中的name字段的值
