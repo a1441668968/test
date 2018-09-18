e6ca0ac5d72530684a9588673a106a7f77eb0fd1
root <root@room9pc01.tedu.cn>
2018-09-18 18:51:29 +0800

    9.18

--- newsite/myapp/urls.py
+++ newsite/myapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', view=views.index, name='index'),
     url(r'^hello/$', view=views.hello, kwargs={'age': 23, }, name='hello'),
-    url(r'^(?P<name>\w+)/(?P<age>\d+)/', view=views.info, name='info')
+    url(r'^(?P<name>\w+)/(?P<age>\d+)/', view=views.info, name='info'),
+    url(r'^home/$', view=views.home, name='home'),
+    url(r'^protect/$', view=views.protect, name='protect'),
+    url(r'^login/$', view=views.login, name='login')
 ]
--- newsite/myapp/views.py
+++ newsite/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 
 # Create your views here.
@@ -16,3 +16,24 @@ def index(request):
     else:
         user = None
     return render(request, 'index.html', {'user': user})
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def login(request):
+    user = request.POST.get('username')
+    passwd = request.POST.get('password')
+    if user == 'tom' and passwd == '123456':
+        request.session['LOGIN'] = True
+        return redirect('protect')
+    else:
+        return redirect('home')
+
+
+def protect(request):
+    is_login = request.session.get('LOGIN', False)
+    if is_login:
+        return render(request=request, template_name='protect.html')
+    return redirect('home')
